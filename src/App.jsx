d9401781c173cfd5856d0b51f53f075ad8e4b908
rodrigo <rodrigopinto5050@gmail.com>
2024-01-27 10:07:11 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons'
+import Inputs from './components/Inputs'
+import TimeLocation from './components/TimeLocation'
+import { TemperatureDetails } from './components/TemperatureDetails'
+import Forecast from './components/Forecast'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-slate-600
+      to-slate-900 h-fit shadow-xl shadow-gray-400'
+    >
+      <TopButtons />
+      <Inputs />
+      <TimeLocation />
+      <TemperatureDetails />
+      <Forecast title="hourly forecast"/>
+      <Forecast title="hourly forecast"/>
+    </div>
+  )
+}
+
+export default App
